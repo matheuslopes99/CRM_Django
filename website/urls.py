727bf86901d852e846e9f6e0ae5bcf626762782e
Matheus Lopes <matheus.lopes52@gmail.com>
2023-09-23 20:02:38 -0300

    registration

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import urls as auth_urls
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    # path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    ]   
